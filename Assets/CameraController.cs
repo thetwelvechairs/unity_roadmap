d5aaa6dcef3750fcf921adfcfbc5a98e0f308126
vokamis <asimakov@twelvechairs-software.com>
2023-12-25 15:41:25 -0500

    Commit

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,22 +1,27 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraController : MonoBehaviour
 {
-    const float sensitivity = 2;
-    const float speed = 10;
+    private const float sensitivity = 4;
+    private const float speed = 10;
+
     private const float camera_y_max = 5.0f;
     private const float camera_y_min = 1.0f;
 
-    void Start()
-    {
-        // Cursor.lockState = CursorLockMode.Locked;
-    }
+    private Ray ray;
+    private RaycastHit hit;
 
     void Update()
     {
         // Move the camera forward, backward, left, and right
         transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        // Honor scene bounds
+        float temp_max_x = MainScene.max_x * 10;
+        float temp_max_z = MainScene.max_z * 10;
+
         if (Camera.main.transform.position.y < camera_y_min)
         {
             Camera.main.transform.position =
@@ -28,34 +33,44 @@ public class CameraController : MonoBehaviour
                 new Vector3(Camera.main.transform.position.x, camera_y_max, Camera.main.transform.position.z);
         }
         
-        if (Camera.main.transform.position.x > MainScene.max_x)
+        if (Camera.main.transform.position.x > temp_max_x)
         {
             Camera.main.transform.position =
-                new Vector3(MainScene.max_x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+                new Vector3(temp_max_x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+        }
+        if (Camera.main.transform.position.x < -temp_max_x)
+        {
+            Camera.main.transform.position =
+                new Vector3(-temp_max_x, Camera.main.transform.position.y, Camera.main.transform.position.z);
         }
         
-        if (Camera.main.transform.position.x < -MainScene.max_x)
+        if (Camera.main.transform.position.z > temp_max_z)
         {
             Camera.main.transform.position =
-                new Vector3(-MainScene.max_x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+                new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, temp_max_z);
         }
-        
-        if (Camera.main.transform.position.z > MainScene.max_z)
+        if (Camera.main.transform.position.z < -temp_max_z)
         {
             Camera.main.transform.position =
-                new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, MainScene.max_z);
-        }
-        
-        if (Camera.main.transform.position.z < -MainScene.max_z)
-        {
-            Camera.main.transform.position =
-                new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, -MainScene.max_z);
+                new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, -temp_max_z);
         }
         
         // Rotate the camera based on the mouse movement
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-        transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0);
-        
+        if (Input.GetMouseButton(1)){
+            // Cursor.lockState = CursorLockMode.Locked;
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+            transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0);
+        }
+
+        else if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                hit.collider.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+            }
+        }
+
     }
 }
--- Assets/MainScene.cs
+++ Assets/MainScene.cs
@@ -4,16 +4,16 @@ using Random = UnityEngine.Random;
 
 public class MainScene : MonoBehaviour
 {
-    const int TEST_teams = 3;
-    const int TEST_n = 10;
+    private const int TEST_teams = 3;
+    private const int TEST_n = 10;
 
-    public const float max_x = 10.0f;
-    public const float max_z = 10.0f;
+    public const float max_x = 4.0f;
+    public const float max_z = 4.0f;
     
     Epic[] epics = new Epic[TEST_n];
     
     GameObject[] objects = new GameObject[TEST_n];
-    
+
     // Color customColor = new Color();
 
     void setupEpics()
@@ -23,7 +23,7 @@ public class MainScene : MonoBehaviour
             Epic temp = new Epic();
             temp.title = "" + i;
             temp.targetStart = i;
-            temp.targetEnd = Random.Range(2, 6);
+            temp.targetEnd = Random.Range(2, 16);
             temp.team_id = Random.Range(1, TEST_teams + 1);
             temp.customColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
 
@@ -39,20 +39,24 @@ public class MainScene : MonoBehaviour
         plane.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
         plane.transform.localScale = new Vector3(max_x, 0.1f, max_z);
         plane.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
-        
+        plane.layer = 2;
 
-        for (int i = 0; i < epics.Length; i++)
+
+        for (int n = 0; n < 6; n++)
         {
-            objects[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            objects[i].transform.localScale = new Vector3(1.0f, 0.3f, epics[i].targetEnd);
-            objects[i].transform.position = new Vector3(0.0f, epics[i].team_id, epics[i].targetStart);
-            objects[i].AddComponent<Rigidbody>();
-            objects[i].GetComponent<Renderer>().material.SetColor("_Color", epics[i].customColor); ;
+            for (int i = 0; i < epics.Length; i++)
+            {
+                objects[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                objects[i].transform.localScale = new Vector3(1.0f, 0.3f, epics[i].targetEnd);
+                objects[i].transform.position = new Vector3(n * 2, epics[i].team_id, epics[i].targetStart);
+
+                objects[i].name = "Plank " + i;
+                objects[i].GetComponent<Renderer>().material.SetColor("_Color", epics[i].customColor);
+                ;
+
+                objects[i].AddComponent<Rigidbody>();
+                objects[i].GetComponent<Rigidbody>().mass = epics[i].targetEnd / 20.0f;
+            }
         }
-    }
-
-    void Update()
-    {
-
     }
 }
